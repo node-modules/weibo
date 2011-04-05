9e21d37caa780d313d04dbe4a9fc6f16e64da948
fengmk2 <fengmk2@gmail.com>
2011-04-06 01:50:37 +0800

    add instapaper api

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib/tapi.js').tapi;
+exports.tapi = require('./lib/tapi').tapi;
+
+exports.oauth_middleware = require('./lib/oauth_middleware');
+
+exports.instapaper = require('./lib/instapaper').instapaper;
