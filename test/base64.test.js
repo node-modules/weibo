7d80bc1bc9804f4fcd0038c6e25c58ea9c23604d
fengmk2 <fengmk2@gmail.com>
2011-06-17 20:37:12 +0800

    update weibo npm

--- test/base64.test.js
+++ test/base64.test.js
@@ -9,7 +9,7 @@ module.exports = {
     'strcode encode': function() {
 		var cases = ['你好', 'json', 'XX你好啊！dawa\';:\"/?.>？》！@＃！¥％⋯⋯—＊（}{"'];
 		for(var i = 0; i < cases.length; i++) {
-			var c = cases[i], key = 'asdfwfewfwfwfwwefwfwefw';
+			var c = cases[i], key = '523f2d0d134bfd5aa138f9e5af828bf9';
 			//console.log(base64.strcode(c, key), base64.strcode(base64.strcode(c, key), key, true));
 			assert.equal(base64.strcode(base64.strcode(c, key), key, true), c);
 		}
