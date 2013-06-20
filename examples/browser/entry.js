ae042da07a37bed4c115fd3c1987660b105c4275
fengmk2 <fengmk2@gmail.com>
2013-06-20 15:58:04 +0800

    fixed #31 README.md 中的 entry.js 无法正常运行

--- examples/browser/entry.js
+++ examples/browser/entry.js
@@ -7,7 +7,7 @@ var oauth_callback_url = 'your callback url';
 weibo.init('weibo', appkey, secret, oauth_callback_url);
 
 var user = { blogtype: 'weibo' };
-var cursor = {count: 20};
+var cursor = {count: 20, source: appkey};
 weibo.public_timeline(user, cursor, function (err, statuses) {
   if (err) {
     console.error(err);
